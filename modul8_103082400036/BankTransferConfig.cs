4cc61dd4c49a2c213d82d74c7bb1c291f11b68a3
IGedePutuArthaJaya99 <arthawinangun05@gmail.com>
2026-04-27 11:48:55 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_103082400036/BankTransferConfig.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+
+namespace modul8_103082400036
+{
+    public class Transfer
+    {
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+    }
+
+    // Class menampung "confirmation"
+    public class Confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+    }
+
+    // Class utama untuk JSON
+    public class ConfigData
+    {
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public List<string> methods { get; set; }
+        public Confirmation confirmation { get; set; }
+    }
+
+    public class BankTransferConfig
+    {
+        public ConfigData config;
+        private const string filePath = "bank_transfer_config.json";
+
+        public BankTransferConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteConfigFile();
+            }
+        }
+
+        private void ReadConfigFile()
+        {
+            string configJsonData = File.ReadAllText(filePath);
+            config = JsonSerializer.Deserialize<ConfigData>(configJsonData);
+        }
+
+        private void SetDefault()
+        {
+            // Set nilai default 
+            config = new ConfigData();
+            config.lang = "en";
+
+            config.transfer = new Transfer();
+            config.transfer.threshold = 25000000;
+            config.transfer.low_fee = 6500;
+            config.transfer.high_fee = 15000;
+
+            config.methods = new List<string> { "RTO (real-time)", "SKN", "RTGS", "BI FAST" };
+
+            config.confirmation = new Confirmation();
+            config.confirmation.en = "yes";
+            config.confirmation.id = "ya";
+        }
+
+        private void WriteConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+    }
+}
--- modul8_103082400036/Program.cs
+++ modul8_103082400036/Program.cs
@@ -1,1 +1,97 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul8_103082400036
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inisialisasi konfigurasi
+            BankTransferConfig btc = new BankTransferConfig();
+
+            // awal
+            if (btc.config.lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer:");
+            }
+            else
+            {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+            }
+
+            int nominal = Convert.ToInt32(Console.ReadLine());
+            int biaya_transfer = 0;
+
+            // Menghitung biaya transfer berdasarkan threshold
+            if (nominal <= btc.config.transfer.threshold)
+            {
+                biaya_transfer = btc.config.transfer.low_fee;
+            }
+            else
+            {
+                biaya_transfer = btc.config.transfer.high_fee;
+            }
+
+            int total_biaya = nominal + biaya_transfer;
+
+            //  Menampilkan total dan metode
+            if (btc.config.lang == "en")
+            {
+                Console.WriteLine($"Transfer fee = {biaya_transfer}");
+                Console.WriteLine($"Total amount = {total_biaya}");
+                Console.WriteLine("Select transfer method:");
+            }
+            else
+            {
+                Console.WriteLine($"Biaya transfer = {biaya_transfer}");
+                Console.WriteLine($"Total biaya = {total_biaya}");
+                Console.WriteLine("Pilih metode transfer:");
+            }
+
+            // Print list method 
+            for (int i = 0; i < btc.config.methods.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {btc.config.methods[i]}");
+            }
+
+            // Menerima input metode dari user
+            Console.ReadLine();
+
+            // Konfirmasi transaksi
+            if (btc.config.lang == "en")
+            {
+                Console.WriteLine($"Please type \"{btc.config.confirmation.en}\" to confirm the transaction:");
+            }
+            else
+            {
+                Console.WriteLine($"Ketik \"{btc.config.confirmation.id}\" untuk mengkonfirmasi transaksi:");
+            }
+
+            string konfirmasi = Console.ReadLine();
+
+            // Validasi konfirmasi
+            if (btc.config.lang == "en")
+            {
+                if (konfirmasi == btc.config.confirmation.en)
+                {
+                    Console.WriteLine("The transfer is completed");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer is cancelled");
+                }
+            }
+            else
+            {
+                if (konfirmasi == btc.config.confirmation.id)
+                {
+                    Console.WriteLine("Proses transfer berhasil");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer dibatalkan");
+                }
+            }
+        }
+    }
+}
